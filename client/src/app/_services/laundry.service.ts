f3f98daeec317c8ebba50f6e722f8b21adb13980
Denis <denismakarov123@mail.ru>
2020-12-20 12:18:48 +0300

    hz

--- client/src/app/_services/laundry.service.ts
+++ client/src/app/_services/laundry.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Laundry } from '@app/_models';
 import { environment } from '@environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,11 @@ import { environment } from '@environments/environment';
 export class LaundryService {
   constructor(private http: HttpClient) { }
 
-  getAll() {
+  getAll(): Observable<Laundry[]> {
     return this.http.get<Laundry[]>(`${environment.apiUrl}/admin/laundries`);
+  }
+
+  getLaundry(id: string): Observable<Laundry> {
+    return this.http.get<Laundry>(`${environment.apiUrl}/admin/laundry/${id}`);
   }
 }
--- client/src/app/admin-laundry/admin-laundry.component.ts
+++ client/src/app/admin-laundry/admin-laundry.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LaundryService } from '@app/_services';
+import {Laundry} from '@app/_models';
 
 @Component({
   selector: 'app-admin-laundry',
-  templateUrl: './admin-laundry.component.html',
-  styleUrls: ['./admin-laundry.component.less']
+  templateUrl: './admin-laundry.component.html'
 })
 export class AdminLaundryComponent implements OnInit {
-
-  constructor() { }
+  laundry: Laundry;
+  constructor(private laundryService: LaundryService) { }
 
   ngOnInit(): void {
   }
